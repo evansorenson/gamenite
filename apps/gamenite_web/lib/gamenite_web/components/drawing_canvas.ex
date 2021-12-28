80355eb15c1d022df31b00bdf5221bc9022f9985
Evan Sorenson <evan.sorenson.9@gmail.com>
2021-12-28 09:52:41 -0600

    added eraser and color changing to pictionary canvas

--- apps/gamenite_web/lib/gamenite_web/components/drawing_canvas.ex
+++ apps/gamenite_web/lib/gamenite_web/components/drawing_canvas.ex
@@ -1,14 +1,28 @@
 defmodule GameniteWeb.Components.DrawingCanvas do
   use Surface.LiveComponent
 
-  prop(user_id, :any, required: true)
-  prop(drawing_user_id, :any, required: true)
-  prop(phrase_to_draw, :string, required: true)
+  prop user_id, :any, required: true
+  prop drawing_user_id, :any, required: true
+  prop phrase_to_draw, :string, required: true
+
+  prop canvas_hex_colors, :list,
+    default: [
+      "000000",
+      "FFFFFF",
+      "964B00",
+      "FF0000",
+      "FF9900",
+      "FFFF00",
+      "00FF00",
+      "00FFFF",
+      "0000FF",
+      "9900FF",
+      "FF00FF"
+    ]
 
   def render(assigns) do
     ~F"""
-
-    <div x-data="{ down: false }">
+    <div x-data="{ down: false }" data-color="'#000000'" data-drawing-type="pen">
       <script>
         function get_canvas_ref() {
           var canvas= document.getElementById('canvas');
@@ -26,15 +40,22 @@ defmodule GameniteWeb.Components.DrawingCanvas do
           return {x: (event.clientX - rect.left) * scaleX, y: (event.clientY - rect.top) * scaleY}
         }
 
-        function mouseDown(e, fill) {
-          if (fill) {
-            //floodFill(event, canvasRef.current, ctxRef.current, brushColor);
-            //dispatch({ type: HANDLE_CANVAS_UPDATE, payload: canvasRef?.current?.toDataURL() });
+        function mouseDown(e, drawing_type, color) {
+          ctx = get_canvas_ref();
+          if (drawing_type == 'fill') {
+            ctx.beginPath();
+            ctx.fillStyle = color;
+            ctx.fill();
             return;
+          }
+          else if (drawing_type == 'eraser') {
+            ctx.globalCompositeOperation = 'destination-out';
+          }
+          else {
+            ctx.globalCompositeOperation = 'source-over';
           }
           let {x, y} = getMousePos(e);
 
-          ctx = get_canvas_ref();
           ctx.beginPath();
           ctx.moveTo(x, y);
         }
@@ -44,14 +65,16 @@ defmodule GameniteWeb.Components.DrawingCanvas do
           ctx.closePath();
         }
 
-        function draw(e, down) {
+        function draw(e, down, color) {
+          console.log(color);
           if (!down) return;
 
           ctx = get_canvas_ref();
 
           let {x, y} = getMousePos(e);
           ctx.lineTo(x, y);
-          ctx.fillStyle = "#FF0000";
+          ctx.fillStyle = color;
+          ctx.strokeStyle = color;
           ctx.lineWidth = 1;
           ctx.stroke();
         }
@@ -62,8 +85,17 @@ defmodule GameniteWeb.Components.DrawingCanvas do
         }
       </script>
 
-      <canvas id="canvas" class="h-full w-full bg-white" @mouseup="down = false; mouseUp()" @mousedown="down = true; mouseDown($event, false)" @mousemove="draw($event, down)"/>
-      <div>
+      <canvas id="canvas" class="h-full w-full bg-white" @mouseup="down = false; mouseUp()" @mousedown="if ($root.dataset.drawing_type != 'fill') { down = true; } mouseDown($event, $root.dataset.drawing_type, $root.dataset.color)" @mousemove="draw($event, down, $root.dataset.color)"/>
+      <div class="flex">
+        {#for color <- @canvas_hex_colors}
+          <button style={"background-color:##{color}"} class="h-12 w-12 border-0 rounded-none" @click={"$root.dataset.color = '##{color}'; $root.dataset.drawing_type = 'pen'"}></button>
+        {/for}
+
+        <button @click="$root.dataset.drawing_type = 'fill'">Fill</button>
+        <button @click="$root.dataset.drawing_type = 'eraser'">Eraser</button>
+
+
+        <button @click="clear()">Clear</button>
       </div>
     </div>
     """
